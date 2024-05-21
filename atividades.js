34be493fa778b767aa4bd826f3ba4fad969059c8
João Piologo <122281207+jpiologo@users.noreply.github.com>
2024-05-21 17:01:26 -0300

    Module 03
    Module 03 application

--- atividades.js
+++ atividades.js
@@ -1,17 +1,21 @@
-//Atividade 01;
+// //Atividade 01;
 
-const htmlConst = document.querySelector('html');
-const tempo = document.querySelector('#timer');
-const img = document.querySelector('.app__image');
-const title = document.querySelector('.app__title');
+// const htmlConst = document.querySelector('html');
+// const tempo = document.querySelector('#timer');
+// const img = document.querySelector('.app__image');
+// const title = document.querySelector('.app__title');
 
-//Buttons
-const startBtn = document.getElementById('start-pause');
-const focoBtn = document.getElementsByClassName('app__card-button--foco');
-const curtoBtn = document.getElementsByClassName('app__card-button--curto');
-const longoBtn = document.getElementsByClassName('app__card-button--longo');
+// //Buttons
+// const startBtn = document.getElementById('start-pause');
+// const focoBtn = document.getElementsByClassName('app__card-button--foco');
+// const curtoBtn = document.getElementsByClassName('app__card-button--curto');
+// const longoBtn = document.getElementsByClassName('app__card-button--longo');
 
-//Timer
-const timeFoco = 1500;
-const timeCurto = 300;
-const timeLongo =  900;
+// //Timer
+// const timeFoco = 1500;
+// const timeCurto = 300;
+// const timeLongo =  900;
+
+// function alteraCnt (contexto){
+//     img.setAttribute('src', `/imagens/${contexto}.png`);
+// }
