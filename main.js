d7c954e1d9eb1efdd9be79e3f26033e0114952f3
João Piologo <122281207+jpiologo@users.noreply.github.com>
2024-05-22 15:36:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
+
+let tempoEmSegundos = 5;
+let intervaloId = null;
+
 musica.loop = true;
 
 musicaFocoInput.addEventListener('change', () => {
@@ -54,3 +59,29 @@ longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo');
     longoBt.classList.add('active');
 })
+
+
+const contagemRegressiva = () => {
+    if(tempoEmSegundos <= 0){
+        zerar();
+        alert('Tempo finalizado');
+        return;
+    }
+    tempoEmSegundos -= 1;
+    console.log(`Temporizador ${tempoEmSegundos}`);
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar(){
+    if(intervaloId){
+        zerar();
+        return;
+    }
+    intervaloId = setInterval(contagemRegressiva, 1000); 
+}
+
+function zerar(){
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
