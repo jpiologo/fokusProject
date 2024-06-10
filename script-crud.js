05e72c5448d702a505bbc15678f655d3a4089dc8
João Piologo <122281207+jpiologo@users.noreply.github.com>
2024-06-10 13:14:15 -0300

    Add files via upload

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,7 @@ const ulTarefas =  document.querySelector('.app__section-task-list');
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
 const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas');
 const btnRemoverTodas = document.querySelector('#btn-remover-todas');
+const btnCancel = document.querySelector('.app__form-footer__button--cancel');
 
 
 let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
@@ -21,10 +22,10 @@ function criarElementoTarefa(tarefa) {
 
     const svg = document.createElement('svg');
     svg.innerHTML = `
-        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-        </svg>
+            <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+                <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+            </svg>
     `
 
     const paragrafo = document.createElement('p');
@@ -79,6 +80,10 @@ function criarElementoTarefa(tarefa) {
 
     return li;
 }
+
+btnCancel.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden');
+})
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden');
